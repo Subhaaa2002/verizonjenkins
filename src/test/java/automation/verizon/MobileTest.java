3d694b39d56d8c8091802990ca089cbfda69c8e6
Subhaaa <subhalakshmisiva@gmail.com>
2023-07-26 14:10:27 +0530

    initial commit

---
+++ src/test/java/automation/verizon/MobileTest.java
@@ -0,0 +1,17 @@
+package automation.verizon;
+
+import org.testng.annotations.Test;
+
+public class MobileTest {
+	@Test
+	void login() {
+		System.out.println("Login");
+	}
+	
+	@Test
+	void logout() {
+		System.out.println("Logout");
+		//int num=10/0;
+	}
+
+}
